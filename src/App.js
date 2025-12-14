9b52c4b00877664df2d59bd65f93171ddec5d1b5
Dawid <termos130@gmail.com>
2025-12-14 12:18:10 +0100

    Ćwiczenie REACT z prawidłowo dodanym komunikatem- Reaktywność + przycisk + lista elementów + dodania niespodzianka jak się wpisze wiecej jak 8 znaków 😅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
-
+import { useState } from 'react';
 
 function App() {
+  const [title, setTitle] = useState('Wall-E');
 
-    const [title, setTitle] = useState('Wall-E');
+  function handleChange(event) {
+    setTitle(event.target.value);
+  }
 
-    function handleChange(event) {
+  let message = '';
 
-        setTitle(event.target.value);
-    }
-
-let message = '';
-if(title.length < 5 ){
-message = 'Title is too short'}
-else if (title.length < 8 ){
-message = 'Title is ok'}
-else {
-message = `
+  if (title.length < 5) {
+    message = 'Title is too short';
+  } else if (title.length < 8) {
+    message = 'Title is ok';
+  } else {
+    message = `
 at constructor (github.com/this-repo-does-not-exist-500/internal-error/build-crash/react-lab/node_modules/@babel/parser/lib/index.js:360:19)
 at FlowParserMixin.raise (github.com/this-repo-does-not-exist-500/internal-error/build-crash/react-lab/node_modules/@babel/parser/lib/index.js:6613:19)
 at FlowParserMixin.semicolon (github.com/this-repo-does-not-exist-500/internal-error/build-crash/react-lab/node_modules/@babel/parser/lib/index.js:6910:10)
@@ -43,26 +41,41 @@ at normalizeFile (github.com/this-repo-does-not-exist-500/internal-error/build-c
 at run (github.com/this-repo-does-not-exist-500/internal-error/build-crash/react-lab/node_modules/@babel/core/lib/transformation/index.js:22:50)
 at step (github.com/this-repo-does-not-exist-500/internal-error/build-crash/react-lab/node_modules/gensync/index.js:261:32)
 at async.call.result.err.err (github.com/this-repo-does-not-exist-500/internal-error/build-crash/react-lab/node_modules/gensync/index.js:223:11)
-`
-}
+`;
+  }
 
-    return (
-        <div>
-            <h1>My favourite movies to watch</h1>
-            <h2>My favourite movie for today is {title}</h2>
-            {
-                       title.length > 0 && <pre className="crash">{message}</pre>
+  const movies = [
+    { title: 'Wall-E' },
+    { title: 'Pulp Fiction' },
+    { title: 'Matrix' },
+    { title: '1670' },
+  ];
 
-                        }
-            <input type="text" onChange={handleChange}/>
-            <button type="button" onClick={() => alert(title)}>
+  return (
+    <div>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.title}>{movie.title}</li>
+        ))}
+      </ul>
 
-    Pokaż tytuł filmu
+      <h1>My favourite movies to watch</h1>
+      <h2>My favourite movie for today is {title}</h2>
 
-</button>
+      {title.length > 0 &&
+        (title.length >= 8 ? (
+          <pre className="crash">{message}</pre>
+        ) : (
+          <div>{message}</div>
+        ))}
 
-        </div>
-    );
+      <input type="text" onChange={handleChange} />
+
+      <button type="button" onClick={() => alert(title)}>
+        Pokaż tytuł filmu
+      </button>
+    </div>
+  );
 }
 
 export default App;
